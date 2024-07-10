35c6b55306ed1cb6fb389c4cc6a51a2a4f808b0a
Ross Giovan <rosseyg@gmail.com>
2024-07-10 09:35:26 -0400

    11ty up
    11tyed it

--- .eleventy.js
+++ .eleventy.js
@@ -18,7 +18,7 @@ module.exports = function(eleventyConfig) {
         input: "src",
         includes: "_includes",
         data: "_data",
-        output: "docs"
+        output: "_site"
       }
     };
   };
