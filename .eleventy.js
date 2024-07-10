fcefeae733fe78bf53b6a3927a9d5e54d9f2fd89
Ross Giovan <rosseyg@gmail.com>
2024-07-10 02:55:12 -0400

    \\\
    \\\

--- .eleventy.js
+++ .eleventy.js
@@ -18,7 +18,7 @@ module.exports = function(eleventyConfig) {
         input: "src",
         includes: "_includes",
         data: "_data",
-        output: "_site"
+        output: "docs"
       }
     };
   };
